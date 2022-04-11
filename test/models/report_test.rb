385a505d1fc26567ae1478ba6a7c0fad69ad7948
daiki0381 <daiki03810218@gmail.com>
2022-04-11 23:06:09 +0900

    reportモデルの単体テスト

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "editable?" do
+    report = reports(:one)
+
+    alice = users(:alice)
+    assert report.editable?(alice)
+
+    bob = users(:bob)
+    assert_not report.editable?(bob)
+  end
+
+  test "created_on" do
+    report = reports(:one)
+    assert_instance_of Date, report.created_on
+  end
 end
