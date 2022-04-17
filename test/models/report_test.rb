bfa9f7484d6888457bcbed2e7a6d226086ea4f4c
daiki0381 <daiki03810218@gmail.com>
2022-04-17 13:37:49 +0900

    created_onの修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -16,5 +16,6 @@ class ReportTest < ActiveSupport::TestCase
   test 'created_on' do
     report = reports(:report_of_alice)
     assert_instance_of Date, report.created_on
+    assert_equal Time.zone.today, report.created_on
   end
 end
