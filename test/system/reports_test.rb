74355b7d9e00ad03c82d85a3418a7d2867ddf1a0
daiki0381 <daiki03810218@gmail.com>
2022-04-17 14:36:13 +0900

    reports_testの修正

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -14,17 +14,21 @@ class ReportsTest < ApplicationSystemTestCase
   test 'visiting the index' do
     visit reports_url
     assert_selector 'h1', text: '日報'
+    assert_selector 'td', text: '「Railsでテストを書く」'
   end
 
   test 'creating a Report' do
     visit reports_url
     click_on '新規作成'
 
-    fill_in 'タイトル', with: @report.title
-    fill_in '内容', with: @report.content
+    fill_in 'タイトル', with: '「フィヨルドブートキャンプへようこそ！」'
+    fill_in '内容', with: '「フィヨルドブートキャンプへようこそ！」の課題に取り組んだ。'
     click_on '登録する'
 
     assert_text '日報が作成されました。'
+    assert_selector 'h1', text: '日報の詳細'
+    assert_selector 'p', text: '「フィヨルドブートキャンプへようこそ！」'
+    assert_selector 'p', text: '「フィヨルドブートキャンプへようこそ！」の課題に取り組んだ。'
     click_on '戻る'
   end
 
@@ -34,16 +38,21 @@ class ReportsTest < ApplicationSystemTestCase
       click_on '編集', match: :first
     end
 
-    fill_in 'タイトル', with: @report.title
-    fill_in '内容', with: @report.content
+    fill_in 'タイトル', with: '「JavaScript入門」'
+    fill_in '内容', with: '「JavaScript入門」の課題に取り組んだ。'
     click_on '更新する'
 
     assert_text '日報が更新されました。'
+    assert_selector 'h1', text: '日報の詳細'
+    assert_selector 'p', text: '「JavaScript入門」'
+    assert_selector 'p', text: '「JavaScript入門」の課題に取り組んだ。'
     click_on '戻る'
   end
 
   test 'destroying a Report' do
     visit reports_url
+    assert_selector 'h1', text: '日報'
+    assert_selector 'td', text: '「Railsでテストを書く」'
     page.accept_confirm do
       click_on '削除', match: :first
     end
